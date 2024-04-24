3e36f4129bbf6b5d8bc4452f0d8f8921808c6e30
Ben Goodair <63054419+BenGoodair@users.noreply.github.com>
2024-04-24 14:18:55 +0100

    baby, I have good intentions

--- data/exits.py
+++ data/exits.py
@@ -26,13 +26,13 @@ exitdata["Places"] = pd.to_numeric(exitdata["Places"], errors="coerce")
 exitdata["childrens_homes"] = 1
 
 exitdata = (
-    exitdata.groupby(["Sector", "year", "leave_join", "Local.authority", "imd_decile"])
+    exitdata.groupby(["Sector", "year", "leave_join", "Local.authority"])
     .agg(childrens_homes=("childrens_homes", "sum"), Places=("Places", "sum"))
     .reset_index()
 )
 
 all = (
-    exitdata.groupby(["Sector", "year", "leave_join", "imd_decile"])
+    exitdata.groupby(["Sector", "year", "leave_join"])
     .agg(childrens_homes=("childrens_homes", "sum"), Places=("Places", "sum"))
     .reset_index()
 )
@@ -44,7 +44,7 @@ exitdata = pd.concat([exitdata, all])
 
 exitdata = pd.melt(
     exitdata,
-    id_vars=["Sector", "Local.authority", "year", "leave_join", "imd_decile"],
+    id_vars=["Sector", "Local.authority", "year", "leave_join"],
     value_vars=["childrens_homes", "Places"],
     var_name="Homes_or_places",
     value_name="value",
@@ -66,7 +66,7 @@ exitdata["value"] = pd.to_numeric(exitdata["value"], errors="coerce")
 
 # Grouping by specified columns and calculating net change
 grouped = (
-    exitdata.groupby(["year", "Local.authority", "Homes_or_places", "Sector", "imd_decile"])
+    exitdata.groupby(["year", "Local.authority", "Homes_or_places", "Sector"])
     .apply(
         lambda x: x.loc[x["leave_join"] == "Entries", "value"].sum()
         - x.loc[x["leave_join"] == "Exits", "value"].sum()
@@ -107,3 +107,99 @@ exitdata["year"] = pd.Categorical(
 )
 
 exitdata.to_csv(csv_path("exits"), index=False, header=True, encoding="utf-8")
+
+
+
+
+
+
+
+
+
+
+import pandas as pd
+from utils import csv_path
+
+exitdata = pd.read_csv(
+    "https://raw.githubusercontent.com/BenGoodair/childrens_social_care_data/main/Final_Data/outputs/enter_exit.csv",
+    encoding="ISO-8859-1",
+)
+
+
+exitdata["Date"] = pd.to_datetime(exitdata["Date"], format="%d/%m/%Y")
+exitdata["year"] = exitdata["Date"].dt.strftime("%Y")
+exitdata["Sector"] = exitdata["Sector"].replace(
+    {
+        "Private": "For-profit",
+        "Local Authority": "Local Authority",
+        "Health Authority": "Local Authority",
+        "Voluntary": "Third Sector",
+    }
+)
+exitdata["leave_join"] = exitdata["leave_join"].replace(
+    {"Leave": "Exits", "Join": "Entries"}
+)
+
+
+exitdata["Places"] = pd.to_numeric(exitdata["Places"], errors="coerce")
+exitdata["childrens_homes"] = 1
+
+exitdata = (
+    exitdata.groupby(["Sector", "leave_join", "Local.authority", "imd_decile"])
+    .agg(childrens_homes=("childrens_homes", "sum"), Places=("Places", "sum"))
+    .reset_index()
+)
+
+all = (
+    exitdata.groupby(["Sector",  "leave_join", "imd_decile"])
+    .agg(childrens_homes=("childrens_homes", "sum"), Places=("Places", "sum"))
+    .reset_index()
+)
+
+all["Local.authority"] = "All"
+
+exitdata = pd.concat([exitdata, all])
+
+
+exitdata = pd.melt(
+    exitdata,
+    id_vars=["Sector", "Local.authority",  "leave_join", "imd_decile"],
+    value_vars=["childrens_homes", "Places"],
+    var_name="Homes_or_places",
+    value_name="value",
+)
+
+exitdata["Homes_or_places"] = exitdata["Homes_or_places"].replace(
+    {"childrens_homes": "Children's homes"}
+)
+
+
+# exitdata.columns.tolist()
+
+
+
+
+# Convert 'value' column to numeric if needed
+exitdata["value"] = pd.to_numeric(exitdata["value"], errors="coerce")
+
+# Grouping by specified columns and calculating net change
+grouped = (
+    exitdata.groupby([ "Local.authority", "Homes_or_places", "Sector", "imd_decile"])
+    .apply(
+        lambda x: x.loc[x["leave_join"] == "Entries", "value"].sum()
+        - x.loc[x["leave_join"] == "Exits", "value"].sum()
+    )
+    .reset_index(name="value")
+)
+
+grouped["leave_join"] = "Net change"
+
+
+# Creating a new DataFrame with the 'Net change' values
+exitdata = pd.concat([exitdata, grouped])
+
+
+exitdata = exitdata.sort_values(by="Local.authority")
+
+exitdata.to_csv(csv_path("exits_imd"), index=False, header=True, encoding="utf-8")
+
